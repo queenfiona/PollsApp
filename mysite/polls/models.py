1ea7a46410c2dcc596d30f3a14710ff7e84bc4d3
queenfiona <fionaityang@gmail.com>
2017-10-16 16:55:57 +0300

    http response

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+import datetime
+from django.utils import timezone
+from django.db import models
+
+class Question(models.Model):
+	question_text = models.CharField(max_length =200)
+	pub_date = models.DateTimeField("Date Published")
+
+	# important for interactive prompt
+	def __str__(self):
+		return self.question_text
+
+	# custom function
+	def was_published_recently(self):
+		return self.pub_date >= (timezone.now() - datetime.timedelta(days=1))
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete =models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
+
